65523b531635129ab6af9dc3a869daaffb330df7
Nana-Seyram <agyemang.nana.b@gmail.com>
2022-04-26 10:25:40 +0000

    implemented uid mappings in the user namespace

--- main.go
+++ main.go
@@ -14,12 +14,36 @@ func main() {
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// flags for the clone command
 		Cloneflags: syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWNET |
 			syscall.CLONE_NEWIPC |
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWUSER,
+
+		// this is to break the mountns' link to the initial ns.
+		// this prevents the sandbox from populating the initial ns' /proc/mount file
+		Unshareflags: syscall.CLONE_NEWNS,
+
+		// mapping uid in the new ns to it's uid in the initial namespace. Unmapped users
+		// are assigned the overflow uid (65534) so we're mapping the container id 0 (root) to
+		// the corresponding uid in the initial namespace.
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 
 	must(cmd.Run())
